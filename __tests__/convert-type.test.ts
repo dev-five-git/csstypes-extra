69845442066f686619833e832527a0efce819fa1
owjs3901 <owjs3901@gmail.com>
2025-11-12 11:13:56 +0900

    Fix test

--- __tests__/convert-type.test.ts
+++ __tests__/convert-type.test.ts
@@ -2,6 +2,9 @@ import { expect, test } from 'bun:test'
 
 import { convertType } from '../convert-type'
 
-test.each([['string', '"string"']])('convertType', (type, expected) => {
-  expect(convertType(type)).toEqual(expected)
-})
+test.each([['string', '"string" | Globals']])(
+  'convertType',
+  (type, expected) => {
+    expect(convertType(type)).toEqual(expected)
+  },
+)
